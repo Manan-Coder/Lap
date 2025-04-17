83461a7e45c0893cba7d982d0b5cdbdfa82a1528
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-04-17 23:25:34 +0530

    some initial files v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-
-
-function App() {
-
-}
+import NavBar from "./components/NavBar.tsx";
+import { BrowserRouter } from "react-router-dom";
+const App = () => (
+  <BrowserRouter>
+<div className="flex flex-col min-h-screen">
+  <NavBar />
+</div></BrowserRouter>
+)
 
 export default App
