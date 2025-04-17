13b2c0612d825e98b228043a9adbe59a6e5da4c4
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-04-18 01:41:30 +0530

    added links in header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,26 @@
 
-import { Github, Linkedin, Mail, Code, Link } from "lucide-react";
+import { Github, Linkedin, Mail, Code } from "lucide-react";
 import { Button } from "react-day-picker";
-
+import {Link} from "react-router-dom"
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   
   return (
     <footer className="mt-auto border-t border-white/10 py-4 bg-space-darker bg-opacity-70 backdrop-blur-md">
       <div className="w-full py-4 px-4 flex flex-row items-center justify-between">
-      <div className="left-0 text-red-50 text-3xl font-bold gap-1 tracking-loose relative">Lap</div>
+      <div className="left-0 text-red-50 text-3xl font-bold gap-1 tracking-loose relative">
+        <Link to={"/"}>
+        Lap
+        </Link>
+      </div>
  
       <div className="flex flex-row gap-5 cursor-pointer">
+        <Link to={"https://github.com/Manan-Coder/Lap"}>
         <Github></Github>
-
+        </Link>
+        <Link to={"mailto:manancoder123@gmail.com"}>
         <Mail></Mail>
+        </Link>
       </div>     
       </div>
     </footer>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,7 +10,9 @@ const NavBar = () => {
     <nav className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 backdrop-blur-md border-b border-white/10 flex justify-between items-center`}>
       <div className="mx-auto px-4 py-4 flex flex-row justify-between items-center w-full">
         <div className="left-0 text-red-50 text-3xl font-bold gap-1 tracking-loose relative">
+          <Link to={"/"}>
             Lap
+            </Link>
         </div>
 
         <div className="flex flex-row justify-evenly gap-5 right-0 items-center size={20} items-center cursor-pointer">
@@ -19,7 +21,8 @@ const NavBar = () => {
             <Github></Github>
             </Link>
             </Button>
-            <Mail></Mail>
+            <Link to={"mailto:manancoder123@gmail.com"}>
+            <Mail></Mail></Link>
         </div>
  
       </div>
