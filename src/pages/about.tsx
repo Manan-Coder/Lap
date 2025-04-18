eb452a150ab40103f751be3c25ad5c35475201c5
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-04-18 21:00:41 +0530

    added dashed borders and that plus signs at the corners design which i saw on the tailwind website

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,15 @@
 const About = () => {
     return(
-        <div></div>
+        <div className="h-full w-full flex items-center justify-center p-8">
+            <div className="py-5 px-5 bg-black max-w-4xl w-full mx-auto rounded-lg flex flex-col gap-4">
+                <div className="text-4xl">
+                   About 
+                </div>
+                <div>
+                    Lap is a coding stats recorder.
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -19,9 +19,18 @@ const Landing = () => {
     return(
         <div className="w-full h-full flex items-center justify-center">
 
-        <div className="flex flex-col items-center justify-between py-5 px-5 border  border-white/10" id="cont">
-            <div className="bg-space-dark flex py-12 px-6 h-full w-full flex-col gap-10 items-center border border-white/10">
-            <Label className="text-2xl">{showLogin ? "Login" : "Signup"}</Label>
+<div className="flex flex-col items-center justify-between py-5 px-5 border border-dashed border-white/10 relative group" id="cont">
+  <span className="absolute -top-2 -left-2 h-4 w-4 bg-space-dark flex items-center justify-center text-white/60">+</span>
+  <span className="absolute -top-2 -right-2 h-4 w-4 bg-space-dark flex items-center justify-center text-white/60">+</span>
+  <span className="absolute -bottom-2 -left-2 h-4 w-4 bg-space-dark flex items-center justify-center text-white/60">+</span>
+  <span className="absolute -bottom-2 -right-2 h-4 w-4 bg-space-dark flex items-center justify-center text-white/60">+</span> 
+  <div className="bg-space-dark flex py-12 px-6 h-full w-full flex-col gap-10 items-center border border-dashed border-white/10 relative group">
+
+  <span className="absolute -top-2 -left-2 h-4 w-4 bg-space-dark flex items-center justify-center text-white/60">+</span>
+  <span className="absolute -top-2 -right-2 h-4 w-4 bg-space-dark flex items-center justify-center text-white/60">+</span>
+  <span className="absolute -bottom-2 -left-2 h-4 w-4 bg-space-dark flex items-center justify-center text-white/60">+</span>
+  <span className="absolute -bottom-2 -right-2 h-4 w-4 bg-space-dark flex items-center justify-center text-white/60">+</span>
+         <Label className="text-2xl">{showLogin ? "Login" : "Signup"}</Label>
             <Input type="email" placeholder="email" name="email" onFocusCapture={focusInp} onBlur={notFocusInp} required></Input>
             <div className="flex flex-row items-center gap-5">
             <Input type= {showpwd ? "text" : "password"} placeholder="password" id="password" onFocusCapture={focusInp} onBlur={notFocusInp} required></Input>
