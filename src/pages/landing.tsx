7f426b123beb1fe79a03e75f935b0de8920e4bc1
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-04-18 12:09:50 +0530

    added borders to the input form for styling

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -6,7 +6,7 @@ const Landing = () => {
     return(
         <div className="w-full h-full flex items-center justify-center">
 
-        <div className="flex flex-col items-center justify-between py-5 px-5">
+        <div className="flex flex-col items-center justify-between py-5 px-5 border  border-white/10">
             <div className="flex py-12 px-6 h-full w-full flex-col gap-10 items-center border border-white/10">
             <Label className="text-2xl">Login</Label>
             <Input placeholder="email"></Input>
