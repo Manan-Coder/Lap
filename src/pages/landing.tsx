6bf9185381b476ccf55b41deae13d1b82622e78d
Manan Sharma <83415076+Manan-Coder@users.noreply.github.com>
2025-04-18 17:12:32 +0530

    added password show/hide functionality

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Eye,EyeClosed } from "lucide-react";
+import { useState } from "react";
 function focusInp(){
 const cont = document.getElementById("cont");
 cont.style.backgroundColor = "rgb(8, 8, 8)"
@@ -10,8 +12,9 @@ const cont = document.getElementById("cont");
 cont.style.backgroundColor = "rgb(15, 17, 18)"
 
 }
+//const [showpwd,hidepwd] = useState(false)
 const Landing = () => {
-
+    const [showpwd,setshowpwd] = useState(false)
     return(
         <div className="w-full h-full flex items-center justify-center">
 
@@ -19,7 +22,12 @@ const Landing = () => {
             <div className="bg-space-dark flex py-12 px-6 h-full w-full flex-col gap-10 items-center border border-white/10">
             <Label className="text-2xl">Login</Label>
             <Input placeholder="email" name="email" onFocusCapture={focusInp} onBlur={notFocusInp}></Input>
-            <Input placeholder="password" name="password" onFocusCapture={focusInp} onBlur={notFocusInp}></Input>
+            <div className="flex flex-row items-center gap-5">
+            <Input type= {showpwd ? "text" : "password"} placeholder="password" id="password" onFocusCapture={focusInp} onBlur={notFocusInp}></Input>
+            <Button onMouseEnter={() => setshowpwd(true)} onMouseLeave={() => setshowpwd(false)} className="bg-space-dark">
+            {showpwd ? <Eye /> : <EyeClosed />}
+            </Button>
+            </div>
             <Button onMouseEnter={focusInp} onMouseLeave={notFocusInp}>Submit</Button>
             </div>
         </div>
